a4166bacbd97e2695e86bc6fbd294524553ba401
Eze-medina <tec.medinaeze@gmail.com>
2025-07-24 22:54:16 -0300

    filter updated

--- src/modules/events/components/filter/Filter.jsx
+++ src/modules/events/components/filter/Filter.jsx
@@ -2,12 +2,20 @@ import style from './filter.module.css'
 import { Search } from "lucide-react"
 
 export const Filter = () => {
+
+  const manejarClick = () => {
+    console.log("¡Hiciste clic!");
+  };
+
+
   return (
     <div className={style.inputCont}>
-      <div className={style.searchCont}>
-        <Search/>
+      <div className={style.element}>
+        <input type="text" placeholder='Search events by name, city, or venue...'/>
+        <button onClick={ manejarClick }>
+          <Search color="rgb(212, 212, 212)"/>
+        </button>
       </div>
-      <input type="text" placeholder='Search events by name, city, or venue...'/>
     </div>
   )
 }
