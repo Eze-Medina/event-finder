03617acf6b32a13c2e922a84413fd43a4ccd6e7d
Eze-medina <tec.medinaeze@gmail.com>
2025-07-25 17:36:44 -0300

    nav updated

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,12 +5,19 @@ export const Nav = () => {
   return (
     <div className={style.container}>
       <div className={style.icon}>
-        <div className={style.appIcon}>
-          <Calendar className={style.calendar} />
-        </div>
+          <Calendar 
+            style={{
+              background: 'linear-gradient(to right, #2c3e50, #2980b9)',
+              padding: '6px',
+              borderRadius: '7px',
+              color: 'white'
+            }}
+            size={20} className={style.calendar} />
         <h1>EventHub</h1>
       </div>
-      <Moon/>
+      <div className={style.theme}>
+        <Moon size={20}/>
+      </div>  
     </div>
   )
 }
